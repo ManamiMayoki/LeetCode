bcfcc086e4c41ccc1156d233f3aed825f1b7d2ba
ManamiMayoki <manamimarufa@gmail.com>
2026-04-29 22:07:16 +0600

    remove duplicates

---
+++ list/26_remove_duplicates.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        i=0
+        while i<len(nums):
+            j=i+1
+            while j<len(nums):
+                if nums[i]==nums[j]:
+                    nums.pop(j)
+                else:
+                    j+=1
+            i+=1
+        return len(nums)
+        
